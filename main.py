610d4a60f4ecb5bf7b657af564724e9e40e55152
alixlm19 <alixlm19@hotmail.com>
2021-01-24 13:33:02 -0500

    Updated root endpoint

--- main.py
+++ main.py
@@ -9,27 +9,28 @@ app = FastAPI()
 loop = asyncio.get_event_loop()
 loop.create_task(iex.start())
 
+@app.get("/")
+async def root():
+    return {"detail": "Not Found"}
 
 @app.get("/company/{company_name}")
 async def get_company(company_name: str):
     tickers = iex.find_ticker(company_name.replace('-', ' ')).values
     return tickers
 
-
 @app.get("/stocks/{ticker}/")
-async def get_company(ticker: str, drange: str = "1m"):
+async def get_company(ticker: str, drange:str="1m"):
     print(drange)
     if not iex.is_valid_ticker(ticker):
         raise HTTPException(status_code=404, detail="Item not found")
-
+    
     data = await iex.get_stock_information(ticker, drange)
     return data
-
 
 @app.get("/crypto/{name}")
 async def get_company(ticker: str):
     if not iex.is_valid_ticker(ticker):
         raise HTTPException(status_code=404, detail="Item not found")
-
+    
     data = await iex.get_stock_information(ticker)
     return data
