96c156d45f75094edaa6d7a0c498834b738d1678
alixlm19 <alixlm19@hotmail.com>
2021-01-24 12:11:35 -0500

    Added range query for stocks api endpoint

--- server.py
+++ server.py
@@ -4,10 +4,10 @@ import asyncio
 from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+# Start IEX API
 loop = asyncio.get_event_loop()
 loop.create_task(iex.start())
-
-# iex.start()
 
 @app.get("/")
 async def root():
@@ -18,7 +18,16 @@ async def get_company(company_name: str):
     tickers = iex.find_ticker(company_name.replace('-', ' ')).values
     return tickers
 
-@app.get("/stocks/{ticker}")
+@app.get("/stocks/{ticker}/")
+async def get_company(ticker: str, drange:str="1m"):
+    print(drange)
+    if not iex.is_valid_ticker(ticker):
+        raise HTTPException(status_code=404, detail="Item not found")
+    
+    data = await iex.get_stock_information(ticker, drange)
+    return data
+
+@app.get("/crypto/{name}")
 async def get_company(ticker: str):
     if not iex.is_valid_ticker(ticker):
         raise HTTPException(status_code=404, detail="Item not found")
